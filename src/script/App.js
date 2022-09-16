390458987eb2e78ad92a03730b1d55733d0cfca5
caiyongqi <caiyongqi@cnpc.com.cn>
2022-09-16 09:54:51 +0800

    modify the structure

--- src/script/App.js
+++ src/script/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../style/App.css';
 import {Button} from 'antd';
 
 function App() {
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../script/App';
 
 test('renders learn react link', () => {
   render(<App />);
