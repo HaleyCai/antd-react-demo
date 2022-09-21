6ed229457ce0581fe8fd49a9041d34f3fac7e531
caiyongqi <caiyongqi@cnpc.com.cn>
2022-09-21 14:15:46 +0800

    9/21_2 完成新增表单的输入框验证函数

--- src/components/UserFormAdd.js
+++ src/components/UserFormAdd.js
@@ -21,18 +21,53 @@ function UserFormAdd (props){
     const key = props.nextKey;
     const defaultValue = defaultNewData(key);
 
-    const validName = (_, value) => {
-        console.log(`输入规则名称为${value}`);
-        // 输入为空值也可通过验证
+    const newDataValidator = (errInfo, value) => {
+        const {field} = errInfo;
+        console.log("field name="+field);
+        console.log(`输入value为${value}`);
+
         if(!value){
-            return Promise.resolve();
+            return Promise.reject("该字段不能为空");
         }
 
-        const match = value.match(/tradeCode \d+/i);
-        if(match){
-            return Promise.resolve();
+        switch(field){
+            case "name":{
+                const match = value.match(/tradeCode \d+/i);
+                if(match){
+                    return Promise.resolve();
+                }else{
+                    return Promise.reject("输入错误，规则名称必须为TradeCode [数字]的格式");
+                }
+            }; 
+            case "callNo":{
+                const inputNum = parseInt(value);
+                if(inputNum){
+                    return Promise.resolve();
+                }else{
+                    return Promise.reject("输入错误，呼叫序号必须为数字");
+                }
+            }; 
+            case "status":{
+                const inputNum = parseInt(value);
+                if(inputNum && (inputNum === 0 || inputNum === 1 || inputNum === 2)){
+                    return Promise.resolve();
+                }else{
+                    return Promise.reject("输入错误，状态只能为0、1、2");
+                }
+            }; 
+            case "progress":{
+                const inputNum = parseInt(value);
+                if(inputNum && (inputNum >= 0 && inputNum <= 100)){
+                    return Promise.resolve();
+                }else{
+                    return Promise.reject("输入错误，进度只能为0-100的数字");
+                }
+            }; 
+            default :
+                return Promise.resolve(); 
+
         }
-        return Promise.reject("输入错误，规则名称必须为TradeCode [数字]的格式");
+        
     }
 
     const handleOk = () => {
@@ -72,22 +107,22 @@ function UserFormAdd (props){
                     <Form.Item name="key" label="序号">
                         <Input disabled={true}></Input>
                     </Form.Item>
-                    <Form.Item name="name" label="规则名称" rules={[{validator: validName}]}>
+                    <Form.Item name="name" label="规则名称" rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：'></Input>
                     </Form.Item>
-                    <Form.Item name="owner" label="所有者">
+                    <Form.Item name="owner" label="所有者"  rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：'></Input>
                     </Form.Item>
-                    <Form.Item name="desc" label="描述">
+                    <Form.Item name="desc" label="描述"  rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：'></Input>
                     </Form.Item>
-                    <Form.Item name="callNo" label="呼叫序号">
+                    <Form.Item name="callNo" label="呼叫序号"  rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：'></Input>
                     </Form.Item>
-                    <Form.Item name="status" label="状态">
+                    <Form.Item name="status" label="状态"  rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：'></Input>
                     </Form.Item>
-                    <Form.Item name="progress" label="进度">
+                    <Form.Item name="progress" label="进度"  rules={[{validator: newDataValidator}]}>
                         <Input placeholder='请输入：' ></Input>
                     </Form.Item>
 
